e00ef5310de514294f231bdd933040ba76cea44e
Uilian Ries <uilianries@gmail.com>
2020-04-30 17:25:28 -0300

    Add cppstd
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 
 
@@ -8,11 +9,35 @@ class JsonRpcCxxConan(ConanFile):
     topics = ("conan", "json-rpc-cxx", "json-rpc")
     url = "https://github.com/bincrafters/conan-json-rpc-cxx"
     homepage = "https://github.com/jsonrpcx/json-rpc-cxx"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    license = "MIT"
+    settings = "compiler"
     no_copy_source = True
     requires = "jsonformoderncpp/3.7.0"
 
-    _source_subfolder = "source_subfolder"
+    @property
+    def _source_subfolder(self):
+        return os.path.join(self.source_folder, "source_subfolder")
+
+    def configure(self):
+        minimal_cpp_standard = "17"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+        minimal_version = {
+            "gcc": "6",
+            "clang": "3.9",
+            "apple-clang": "10",
+            "Visual Studio": "17"
+        }
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler standard version support." % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s." % (self.name, minimal_cpp_standard))
+            return
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s." % (self.name, minimal_cpp_standard))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
